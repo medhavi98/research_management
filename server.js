2e27d753e22186504feca666e2ea1ab8f3692ebc
Lakshika <lakshikamedhavini@gmail.com>
2022-04-21 01:48:59 +0530

    project initialized

---
+++ server.js
@@ -0,0 +1,32 @@
+//importing dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+//creating express app
+const app = express();
+require("dotenv").config();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+
+//configuring dotenv variables
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+//creating express server
+app.listen(PORT, async () => {
+    
+  //mongoDB connection
+  try {
+     mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }); 
+
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Express server running at PORT ${PORT}`);
+});
+
