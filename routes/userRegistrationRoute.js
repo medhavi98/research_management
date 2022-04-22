bb5942b6d0987526178c9a02cc220d01f2ad0918
Lakshika <lakshikamedhavini@gmail.com>
2022-04-23 00:29:10 +0530

    bcrypt hash

--- routes/userRegistrationRoute.js
+++ routes/userRegistrationRoute.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 const { response } = require('express');
 const userDetails = require('../models/userRegistrationModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+router.post('/' , (req,res) => {
+    bcrypt.hash(req.body.password , 10).then(hash => {
+
+        const details = new userDetails({
+            fullName : req.body.fullName,
+            sliitEmail : req.body.sliitEmail,
+            personalEmail : req.body.personalEmail,
+            
+            phone : req.body.phone,
+
+        })
+    })
+})
