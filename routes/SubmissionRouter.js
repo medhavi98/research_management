0be70f87ee6d9450f2db08718a57c30655c66da1
pasindubhasura <srmpasindubhasura@gmail.com>
2022-05-14 23:53:28 +0530

    create get submisison endpoint

--- routes/SubmissionRouter.js
+++ routes/SubmissionRouter.js
@@ -2,7 +2,7 @@ const SubmissionModel = require('../models/SubmissionModal');
 
 const submissionRouter = require('express').Router();
 
-//add mark details
+//add submission details
 submissionRouter.post('/', async (req,res) => {
     const { groupId, submissionType, department } = req.body.submissionDetails;
     const submission = new SubmissionModel({ groupId, submissionType, department });
@@ -15,6 +15,16 @@ submissionRouter.post('/', async (req,res) => {
     }
 })
 
+//get submission details
+submissionRouter.get('/getSubmissions/:groupId', async (req,res) => {
+    const { groupId } = req.params;
 
+    try {
+        const submissions = await SubmissionModel.find({groupId});
+        res.status(200).json({submissions});
+    } catch (error) {
+        res.status(400).json("Submissions retrieving failed");
+    }
+})
 
 module.exports = submissionRouter;
