278d7b32aefe67671e7a77b1a96011fe2897e636
pasindubhasura <srmpasindubhasura@gmail.com>
2022-05-14 18:19:33 +0530

    create edit request details endpoint

--- routes/RequestRoute.js
+++ routes/RequestRoute.js
@@ -13,4 +13,16 @@ requestRouter.post('/', async (req,res) => {
     }
 })
 
+requestRouter.post('/edit/:requestId', async (req,res) => {
+    const { requestId } = req.params;
+    const { supervisor, isAccepted, topicName, topicDescription, review } = req.body.requestDetails;
+    const editedRequest = { supervisor, isAccepted, topicName, topicDescription, review };
+
+    try {
+        await RequestModel.findOneAndUpdate({_id: requestId}, editedRequest);;
+        res.status(200).json("Request details updated successfully");
+    } catch (error) {
+        res.status(400).json("Request details updated failed");
+    }
+})
 module.exports = requestRouter;
