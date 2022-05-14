b3d46c814784aa9d78c752f690218dd4d69c346b
pasindubhasura <srmpasindubhasura@gmail.com>
2022-05-14 16:24:31 +0530

    change group model

--- models/GroupModel.js
+++ models/GroupModel.js
@@ -4,27 +4,21 @@ const { Schema } = mongoose;
 const groupSchema = new Schema({
     groupName : {
         type: String,
-        required: true
     },
     studentIds: {
         type: [String],
-        required: true,
     },
     groupLeaderId: {
         type: String,
-        required: true,
     },
     supervisorId: {
         type: String,
-        required: true,
     },
     coSupervisorId: {
         type: String,
-        required: true,
     },
     panelMemberIds: {
         type: [String],
-        required: true,
     },
 }, {collection : 'groups'})
 
