87ec912e790f7c0efa5a77f5a359eebbd7d756eb
D.I.P Lakshan <onlineipl18@gmail.com>
2022-05-23 15:58:09 +0530

    Doc submision

---
+++ frontend/components/Student/DocumentSubmission/Resources.js
@@ -0,0 +1,55 @@
+import { Button, Grid, Typography } from "@mui/material";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+const Resources = () => {
+  return (
+    <>
+      <Grid>
+        <Grid container>
+          <Grid item md={8}>
+            <Typography variant="h6">Introduction to the Research</Typography>
+            <Typography variant="h6" mt={4}>
+              Learn how to create a SRS
+            </Typography>
+            <Typography variant="h6" mt={4}>
+              Sample charter document
+            </Typography>
+          </Grid>
+          <Grid item md={4}>
+            <Grid>
+              <Button
+                variant="contained"
+                color="success"
+                component="label"
+                startIcon={<DriveFolderUploadIcon />}
+              >
+                <input type="file" />
+              </Button>
+            </Grid>
+            <Grid mt={3}>
+              <Button
+                variant="contained"
+                color="success"
+                component="label"
+                startIcon={<DriveFolderUploadIcon />}
+              >
+                <input type="file" />
+              </Button>
+            </Grid>
+            <Grid mt={3}>
+              <Button
+                variant="contained"
+                color="success"
+                component="label"
+                startIcon={<DriveFolderUploadIcon />}
+              >
+                <input type="file" />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Resources;
