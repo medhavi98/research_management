57421336e6206f6eeb4c6c746044a6b094ad9be8
Charith Widanapathirana <wcharithbuddhipriya@gmail.com>
2022-05-24 11:11:49 +0530

    remove dashboad text

--- frontend/components/DashBoard.js
+++ frontend/components/DashBoard.js
@@ -5,14 +5,7 @@ const DashBoard = () => {
 
   return (
     <Container>
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
-        lorem ipsum dolor sit amet, consectet adip eu, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquy   auctor. Ut enim ad minim veniam, quis nostrud exer method   nulla pariatur. Excepteur sint occaecat cupid par   lobortis nisl, sed diam nonumy eirmod tempor inc           
+
     </Container>
   )
 }
