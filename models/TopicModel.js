0c93ff56d199c3b28bf71b287100c47fc2c976db
Charith Widanapathirana <wcharithbuddhipriya@gmail.com>
2022-05-24 11:10:31 +0530

    Update topic model

--- models/TopicModel.js
+++ models/TopicModel.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const topicSchema = new Schema({
+    submissionType: {
+        type: String,
+    },
     supervisorId: {
         type: String,
     },
