7ced17efb515e051c6481e223351b299aa5c5705
pasindubhasura <srmpasindubhasura@gmail.com>
2022-05-29 15:01:24 +0530

    create message routes and made ui changes

--- frontend/components/Student/Chat/Messenger.js
+++ frontend/components/Student/Chat/Messenger.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Container,
     Grid,
@@ -14,40 +14,52 @@ import {
     TextField,
 
 } from "@mui/material";
-
+import { io } from "socket.io-client";
+import { getUserSessionDetails } from '../../../helpers/userSessionHandler';
+import axios from 'axios';
+import { BASE_URL } from '../../constants';
 
 const Messenger = () => {
+    const socket = io("http://localhost:5001/");
+    const { userId } = getUserSessionDetails();
 
-    const messages = [
-        {
-            _id: "MS0000012",
-            group: "AF_1",
-            sender: "Lakshan",
-            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-        },
-        {
-            _id: "MS0048798",
-            group: "AF_2",
-            sender: "Pasindu",
-            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
-        {
-            _id: "MS00012647",
-            group: "AF_3",
-            sender: "Lakshika",
-            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
-        {
-            _id: "MS00105648",
-            group: "AF_4",
-            sender: "Charith",
-            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        }
-    ]
+    // const messages = [
+    //     {
+    //         _id: "MS0000012",
+    //         group: "AF_1",
+    //         senderId: "628e4ef3062b6c5039ed6f10",
+    //         sender: "Lakshan",
+    //         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    //     },
+    //     {
+    //         _id: "MS0048798",
+    //         group: "AF_2",
+    //         senderId: "628e4ef3062b6c5039ed6f11",
+    //         sender: "Pasindu",
+    //         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    //     },
+    //     {
+    //         _id: "MS00012647",
+    //         group: "AF_3",
+    //         senderId: "628e4ef3062b6c5039ed6f13",
+    //         sender: "Lakshika",
+    //         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    //     },
+    //     {
+    //         _id: "MS00105648",
+    //         group: "AF_4",
+    //         senderId: "628e4ef3062b6c5039ed6f12",
+    //         sender: "Charith",
+    //         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    //     }
+    // ]
 
     const [open, setOpen] = React.useState(false);
+    const [userGroupIds, setUserGroupIds] = React.useState([]);
     const [scroll, setScroll] = React.useState('paper');
     const [send_message, setSend_message] = useState("")
+    const [messages, setMessages] = useState([]);
+    const [messageLength, setMessageLength] = useState(0);
 
     const handleClickOpen = (scrollType) => () => {
         setOpen(true);
@@ -68,11 +80,58 @@ const Messenger = () => {
         }
     }, [open]);
 
+    useEffect(() => {
+        userGroupIds.map((group) => {
+            socket.on(`messageTo${group}`, ({ messageData }) => {
+                console.log(messageData);
+                // alert(messageData.message);
+            })
+        })
+    }, [userGroupIds])
+
+    useEffect(() => {
+        fetchUserGroups();
+    }, [])
+
+    const fetchUserGroups = async () => {
+        const response = await axios.get(`${BASE_URL}/user/getGroupIds/${userId}`);
+        setUserGroupIds(response.data.groupIds);
+
+        const groupIds = JSON.stringify(response.data.groupIds);
+        const msgResponses = await axios.get(`${BASE_URL}/messages/getMessagesByGroup/${groupIds}`);
+        setMessages(msgResponses.data.messages)
+        setMessageLength(msgResponses.data.messageLength);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+    const renderChatCards = messages => {
+        return messages[1].map((msg, index) => {
+            if (index === 0) {
+                return (
+                    <Card onClick={handleClickOpen('paper')} className='chat-card'>
+                        {
+                            <div className='card-content'>
+                                <Grid item md={2}>
+                                    <Avatar className='avatar' aria-label="user">
+                                        {/* {item._id} */}
+                                    </Avatar>
 
+                                </Grid>
+                                <Grid item md={10}>
+                                    <Typography className='message-sender' variant="h5">{msg.senderName}</Typography>
+                                    <Typography className='message-body' sx={{ mt: 2 }}>{msg.message}</Typography>
+                                </Grid>
+                            </div>
+                        }
+                    </Card>
+                );
+            } else {
+                return <></>;
+            }
+        })
+    }
 
     return (
         <Container>
@@ -82,32 +141,13 @@ const Messenger = () => {
                 <hr />
 
             </Grid>
-            <Grid container md={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid container md={12} sx={{ display: 'flex', justifyContent: 'center', }}>
                 <Grid item className='chat-layout'>
-                    <div>
-                        {messages &&
-                            messages.map((item) => {
-                                return (
-                                    <>
-                                        <Card onClick={handleClickOpen('paper')} key={item._id} className='chat-card'>
-                                            <div className='card-content'>
-                                                <Grid item md={2}>
-                                                    <Avatar className='avatar' aria-label="user">
-                                                        {item.group}
-                                                    </Avatar>
-
-                                                </Grid>
-                                                <Grid item md={10}>
-                                                    <Typography className='message-sender' variant="h5">{item.sender}</Typography>
-                                                    <Typography className='message-body' sx={{ mt: 2 }}>{item.message}</Typography>
-                                                </Grid>
-                                            </div>
-                                        </Card>
-                                        <br />
-                                    </>
-                                )
-                            })}
-                    </div>
+                    {messageLength > 0 && (
+                        messages.map((i) => {
+                            return renderChatCards(i);
+                        })
+                    )}
                 </Grid>
             </Grid>
 
@@ -128,44 +168,20 @@ const Messenger = () => {
                             ref={descriptionElementRef}
                             tabIndex={-1}
                         >
-                            <div className="message-card-group">
-                                <Card className="message-card-left">
-                                    <Typography variant='h6'>
-                                        {messages[0].sender}
-                                    </Typography>
-                                    <Typography variant='p'>
-                                        {messages[0].message}
-                                    </Typography>
-                                </Card><br />
-
-                                <Card className="message-card-right">
-                                    <Typography variant='h6'>
-                                        You
-                                    </Typography>
-                                    <Typography variant='p'>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    </Typography>
-                                </Card>
-                            </div>
-                            <div className="message-card-group">
-                                <Card className="message-card-left">
-                                    <Typography variant='h6'>
-                                        {messages[0].sender}
-                                    </Typography>
-                                    <Typography variant='p'>
-                                        {messages[0].message}
-                                    </Typography>
-                                </Card><br />
-
-                                <Card className="message-card-right">
-                                    <Typography variant='h6'>
-                                        {messages[1].sender}
-                                    </Typography>
-                                    <Typography variant='p'>
-                                        {messages[1].message}                                        
-                                    </Typography>
-                                </Card>
-                            </div>
+                            {/* {messages.map((message, index) => {
+                                return (
+                                    <div className="message-card-group">
+                                        <Card className={userId === message.senderId ? "message-card-right" : "message-card-left"}>
+                                            <Typography variant='h6'>
+                                                {message.sender}
+                                            </Typography>
+                                            <Typography variant='p'>
+                                                {message.message}
+                                            </Typography>
+                                        </Card>
+                                    </div>
+                                );
+                            })} */}
                         </DialogContentText>
                     </DialogContent>
                     <DialogActions style={{ padding: '20px' }}>
@@ -193,7 +209,7 @@ const Messenger = () => {
                 </Dialog>
             </div>
 
-        </Container>
+        </Container >
     )
 }
 
--- routes/MessageRouter.js
+++ routes/MessageRouter.js
@@ -1,9 +1,10 @@
 const MessageModel = require('../models/MessageModal');
-
+const _ = require('lodash');
 const messageRouter = require('express').Router();
+const userDetails = require('../models/UserModel');
 
 //add messages
-messageRouter.post('/', async (req,res) => {
+messageRouter.post('/', async (req, res) => {
     const { groupId, senderId, receiverId, message } = req.body.message;
 
     const messageDoc = await new MessageModel({ groupId, senderId, receiverId, message });
@@ -17,12 +18,50 @@ messageRouter.post('/', async (req,res) => {
 })
 
 //get messages
-messageRouter.get('/getMessages/:receiverId', async (req,res) => {
+messageRouter.get('/getMessages/:receiverId', async (req, res) => {
     const { receiverId } = req.params;
 
     try {
-        const messages = await MessageModel.find({receiverId});
-        res.status(200).json({messages});
+        const messages = await MessageModel.find({ receiverId });
+        res.status(200).json({ messages });
+    } catch (error) {
+        res.status(400).json("Messages retrieving failed");
+    }
+})
+
+//get messages
+messageRouter.get('/getMessages/:receiverId', async (req, res) => {
+    const { receiverId } = req.params;
+
+    try {
+        const messages = await MessageModel.find({ receiverId });
+        res.status(200).json({ messages });
+    } catch (error) {
+        res.status(400).json("Messages retrieving failed");
+    }
+})
+
+//get messages by groupId
+messageRouter.get('/getMessagesByGroup/:groupIds', async (req, res) => {
+    const { groupIds } = req.params;
+    const groupIdsArr = JSON.parse(groupIds);
+
+    try {
+        let messages = await MessageModel.find({ groupId: { $in: groupIdsArr } }).sort({ "_id": -1 });
+        const users = await userDetails.find();
+        messages.map((msg, index) => {
+            const user = _.find(users, item => {
+                return item._id.toString() === msg.senderId;
+            });
+            messages[index] = { ...messages[index].toObject(), senderName: user.fullName };
+        });
+        if (messages) {
+            const grouped = _.mapValues(_.groupBy(messages, 'groupId'));
+            const arr = Object.keys(grouped).map((key, index) => [index, grouped[key]]);
+            res.status(200).json({ messages: arr, messageLength: messages.length });;
+        } else {
+            res.status(200).json([]);
+        }
     } catch (error) {
         res.status(400).json("Messages retrieving failed");
     }
