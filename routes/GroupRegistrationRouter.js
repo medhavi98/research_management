8357730a525c62fbfdd090df2c1068172ee5aa46
lakshika <lakshikamedhavini@gmail.com>
2022-05-30 15:38:17 +0530

    group registration post done

--- routes/GroupRegistrationRouter.js
+++ routes/GroupRegistrationRouter.js
@@ -16,27 +16,60 @@ groupRegistrationRouter.post("/", async (req, res) => {
   let groupName = `AF_Group_${count + 1}`;
 
   try {
+    console.log("studentIds", studentIds);
     let stdID = [];
-    studentIds.forEach((element) => {});
-    const { stdIds } = await UserModel.findOne();
-  } catch (err) {}
 
-  //TODO:- get student ids form user collection
-  const group = await new GroupModel({
-    groupName,
-    studentIds,
-    groupLeaderId,
-    supervisorId,
-    coSupervisorId,
-    panelMemberIds,
-  });
+    for (let i = 0; i < studentIds.length; i++) {
+      let id = await UserModel.findOne({
+        studentId: studentIds[i],
+      });
+      console.log("studentId", id._id);
+      stdID.push(id._id);
+    }
 
-  try {
-    await group.save();
-    res.status(200).json("Group registration completed");
-  } catch (error) {
-    res.status(400).json("Group registration failed");
+    const group = new GroupModel({
+      groupName: groupName,
+      studentIds: stdID,
+      groupLeaderId: stdID[0],
+      supervisorId,
+      coSupervisorId,
+      panelMemberIds,
+    });
+
+    console.log("group", group);
+
+    await group
+      .save()
+      .then((response) => {
+        console.log("Group Details Saved", response);
+        res.status(200).json(response);
+      })
+      .catch((error) => {
+        console.log("Group Details Saving error", error);
+        res.status(500).json(error);
+      });
+    console.log("stdID", stdID);
+  } catch (err) {
+    console.error("error while requesting ", err);
+    res.status(500).json(error);
   }
+
+  // //TODO:- get student ids form user collection
+  // const group = await new GroupModel({
+  //   groupName,
+  //   studentIds,
+  //   groupLeaderId,
+  //   supervisorId,
+  //   coSupervisorId,
+  //   panelMemberIds,
+  // });
+
+  // try {
+  //   await group.save();
+  //   res.status(200).json("Group registration completed");
+  // } catch (error) {
+  //   res.status(400).json("Group registration failed");
+  // }
 });
 
 //edit group details
