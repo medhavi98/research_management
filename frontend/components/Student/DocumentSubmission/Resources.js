84afaa79a41456a6d12ace1a9722f631c3f21fef
lakshika <lakshikamedhavini@gmail.com>
2022-06-03 20:38:12 +0530

    std resources admin

--- frontend/components/Student/DocumentSubmission/Resources.js
+++ frontend/components/Student/DocumentSubmission/Resources.js
@@ -1,9 +1,56 @@
-import { Button, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Grid, Typography } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import axios from "axios";
+
 const Resources = () => {
+  const [submittedDocuments, setSubmittedDocuments] = useState([]);
+
+  useEffect(() => {
+    console.log("hhgf");
+    getStudentResources();
+  }, []);
+
+  const getStudentResources = async () => {
+    await axios
+      .get("http://localhost:5001/adminDocumentUploadRouter/studentResources")
+      .then((response) => {
+        console.log(response);
+        setSubmittedDocuments(response.data);
+      })
+      .catch((error) => {
+        console.log("err", error);
+      });
+  };
   return (
     <>
-      <Grid>
+      {submittedDocuments ? (
+        submittedDocuments.map((doc, index) => {
+          console.log("doc " + doc.submissionTitle);
+          return (
+            <>
+              <Card variant="outlined" className="uploaded-card" key={doc._id}>
+                <Typography variant="h6" mt={1} mb={1} ml={2} mr={1}>
+                  {doc.submissionTitle}
+                </Typography>
+                <div className="uploaded-card-Btn">
+                  <a href={doc.templateFile}>
+                    <Button>
+                      <DownloadIcon />
+                    </Button>
+                  </a>
+                </div>
+              </Card>
+              <br />
+            </>
+          );
+        })
+      ) : (
+        <label>No Resources From Admin</label>
+      )}
+
+      {/* <Grid>
         <Grid container>
           <Grid item md={8}>
             <Typography variant="h6">Submit research topic</Typography>
@@ -47,7 +94,7 @@ const Resources = () => {
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Grid> */}
     </>
   );
 };
