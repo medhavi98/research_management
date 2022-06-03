2879b351ccdf788efb5501920f9385ceaf8a5518
lakshika <lakshikamedhavini@gmail.com>
2022-06-03 20:35:54 +0530

    admin doc submissions

--- frontend/components/Admin/DocumentSubmission/AdminDocSubmissions.js
+++ frontend/components/Admin/DocumentSubmission/AdminDocSubmissions.js
@@ -4,20 +4,42 @@ import React, { useState } from "react";
 import { Container } from "@mui/material";
 import { storage } from "../../../firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import TextFieldComponent from "../../Common/TextFieldComponent";
+import CheckBox from "../../Common/CheckBox";
+import "../../Student/DocumentSubmission/SubmissionsStyles.css";
+
 import axios from "axios";
 const AdminDocSubmissions = () => {
   const [ResearchUploadFile, setResearchUploadFile] = useState();
   const [rTemplate, setRTemplate] = useState({});
+  const [submissionTitle, setSubmissionTitle] = useState();
+  const [uploadType, setUploadType] = useState([]);
 
-  const submitResearchDoc = () => {
+  const category = [{ checkBoName: "Student" }, { checkBoName: "Staff" }];
+
+  const staffTypeHandler = (type) => {
+    if (!uploadType.includes(type)) {
+      setUploadType([...uploadType, type]);
+    } else {
+      const newArray = uploadType.filter((value) => {
+        return value !== type;
+      });
+      setUploadType(newArray);
+    }
+  };
+
+  const submitResearchDoc = (e) => {
+    e.preventDefault();
     const researchDetails = {
       templateFile: ResearchUploadFile,
+      submissionTitle: submissionTitle,
+      permissions: uploadType,
     };
     console.log("ruf", researchDetails);
 
     axios
       .post(`http://localhost:5001/adminDocumentUploadRouter`, researchDetails)
-      .then(() => {
+      .then((response) => {
         window.alert("File Uploaded to database!");
       })
       .catch((err) => {
@@ -25,7 +47,8 @@ const AdminDocSubmissions = () => {
       });
   };
 
-  const saveResearchDoc = () => {
+  const saveResearchDoc = (e) => {
+    e.preventDefault();
     console.log("called button submitResearchDoc: ", rTemplate);
     const date = Date.now();
     console.log("u");
@@ -45,7 +68,7 @@ const AdminDocSubmissions = () => {
         setTimeout(() => {
           console.log("ResearchUploadFile 2", ResearchUploadFile);
           // setTimeout(submitResearchDoc(), 2000);
-          submitResearchDoc();
+          submitResearchDoc(e);
           window.alert("Url saved to firebase");
         }, 6000);
       });
@@ -54,7 +77,51 @@ const AdminDocSubmissions = () => {
 
   return (
     <>
-      <Grid>
+      <form onSubmit={(e) => saveResearchDoc(e)}>
+        <TextFieldComponent
+          label="Upload Title"
+          inputName="submissionTitle"
+          classes="form-field"
+          width="100%"
+          inputValue={submissionTitle}
+          required
+          handleChange={(e) => setSubmissionTitle(e.target.value)}
+        />
+        <br />
+        <br />
+        <label>Set Access permissions</label>
+        <CheckBox
+          options={category}
+          onChange={(e) => staffTypeHandler(e.target.value)}
+        />
+
+        <br />
+        <label>Upload your document below</label>
+        <br />
+        <input
+          type="file"
+          onChange={(e) => {
+            console.log("e is : ", e);
+            if (e.target.files[0]) {
+              console.log("rTemplate :", e.target.files[0]);
+              setRTemplate(e.target.files[0]);
+            }
+          }}
+        />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          color="success"
+          type="submit"
+          sx={{ float: "right" }}
+        >
+          Submit
+        </Button>
+        <br />
+        <br />
+      </form>
+      {/* <Grid>
         <Grid container>
           <Grid item md={8}>
             <Typography variant="h6">Introduction to the Research</Typography>
@@ -145,7 +212,7 @@ const AdminDocSubmissions = () => {
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Grid> */}
     </>
   );
 };
