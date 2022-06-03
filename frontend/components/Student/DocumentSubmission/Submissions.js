3ceae8a077acfe2a7ec86511a782ca7ed73cdb38
lakshika <lakshikamedhavini@gmail.com>
2022-06-03 20:38:32 +0530

    submissionss

--- frontend/components/Student/DocumentSubmission/Submissions.js
+++ frontend/components/Student/DocumentSubmission/Submissions.js
@@ -1,19 +1,48 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Card, Grid, Typography } from "@mui/material";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "@mui/material";
 import { storage } from "../../../firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import TextFieldComponent from "../../Common/TextFieldComponent";
 import axios from "axios";
+import "./SubmissionsStyles.css";
+import DownloadIcon from "@mui/icons-material/Download";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const Submissions = () => {
   const [ResearchUploadFile, setResearchUploadFile] = useState();
   const [submissionTitle, setSubmissionTitle] = useState();
+  const [submittedDocuments, setSubmittedDocuments] = useState([]);
   const [rTemplate, setRTemplate] = useState({});
+  const [id, setId] = useState();
+  useEffect(() => {
+    setId(localStorage.getItem("userId"));
+    getGroupDocuments();
+  }, []);
 
-  const submitResearchDoc = () => {
+  const getGroupDocuments = () => {
+    axios
+      .get(
+        `http://localhost:5001/groups/getGroupDocuments/${localStorage.getItem(
+          "userId"
+        )}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setSubmittedDocuments(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const submitResearchDoc = (e) => {
+    e.preventDefault();
+    console.log("submissionTitle : ", submissionTitle);
     const researchDetails = {
       templateFile: ResearchUploadFile,
+      submissionTitle: submissionTitle,
       id: localStorage.getItem("userId"),
     };
     console.log("ruf", researchDetails);
@@ -28,7 +57,22 @@ const Submissions = () => {
       });
   };
 
-  const saveResearchDoc = () => {
+  const deleteSubmission = async (id) => {
+    console.log("document id : ", id);
+    await axios
+      .delete(
+        `http://localhost:5001/fileUploadstd/${id}/${localStorage.getItem(
+          "userId"
+        )}`
+      )
+      .then((response) => {
+        console.log("response : ", response);
+        alert("Document Deleted");
+      });
+  };
+
+  const saveResearchDoc = (e) => {
+    e.preventDefault();
     console.log("called button submitResearchDoc: ", rTemplate);
     const date = Date.now();
     console.log("u");
@@ -47,7 +91,7 @@ const Submissions = () => {
 
         setTimeout(() => {
           console.log("ResearchUploadFile 2", ResearchUploadFile);
-          submitResearchDoc();
+          submitResearchDoc(e);
           //setTimeout(submitResearchDoc(), 2000);
           window.alert("Uploaded to Firebase");
         }, 6000);
@@ -56,12 +100,12 @@ const Submissions = () => {
   };
 
   const onSubmit = (e) => {
-    console.log("value", submissionTitle);
+    alert("value", submissionTitle);
   };
 
   return (
     <>
-      <form onSubmit={(e) => onSubmit(e)}>
+      <form onSubmit={(e) => saveResearchDoc(e)}>
         <TextFieldComponent
           label="Submission Title"
           inputName="submissionTitle"
@@ -71,7 +115,22 @@ const Submissions = () => {
           required
           handleChange={(e) => setSubmissionTitle(e.target.value)}
         />
-
+        <br />
+        <br />
+        <label>Upload your document below</label>
+        <br />
+        <input
+          type="file"
+          onChange={(e) => {
+            console.log("e is : ", e);
+            if (e.target.files[0]) {
+              console.log("rTemplate :", e.target.files[0]);
+              setRTemplate(e.target.files[0]);
+            }
+          }}
+        />
+        <br />
+        <br />
         <Button
           variant="contained"
           color="success"
@@ -81,6 +140,37 @@ const Submissions = () => {
           Submit
         </Button>
       </form>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <label className="uploaded-label">Uploaded Documents</label>
+      <br />
+      <br />
+      {submittedDocuments.map((doc, index) => {
+        console.log("doc " + doc.submissionTitle);
+        return (
+          <>
+            <Card variant="outlined" className="uploaded-card" key={doc._id}>
+              <Typography variant="h6" mt={1} mb={1} ml={2} mr={1}>
+                {doc.submissionTitle}
+              </Typography>
+              <div className="uploaded-card-Btn">
+                <a href={doc.templateFile}>
+                  <Button>
+                    <DownloadIcon />
+                  </Button>
+                </a>
+                <Button onClick={() => deleteSubmission(doc._id)}>
+                  <DeleteIcon />
+                </Button>
+              </div>
+            </Card>
+            <br />
+          </>
+        );
+      })}
+      <br />
 
       {/* <Grid>
         <Grid container>
@@ -120,7 +210,7 @@ const Submissions = () => {
                 Submit
               </button>
             </Grid>
-            {/* <Grid mt={3}>
+            <Grid mt={3}>
               <Button
                 variant="contained"
                 color="success"
@@ -145,8 +235,8 @@ const Submissions = () => {
               >
                 Submit
               </button>
-            </Grid> */}
-      {/* <Grid mt={3}>
+            </Grid>
+            <Grid mt={3}>
               <Button
                 variant="contained"
                 color="success"
@@ -170,10 +260,10 @@ const Submissions = () => {
               >
                 Submit
               </button>
-            </Grid> */}
-      {/* </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>  */}
+      </Grid> */}
     </>
   );
 };
