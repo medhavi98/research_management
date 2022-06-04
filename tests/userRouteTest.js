233e5ba1b8b25c9d6655414e5f514a67e9fb6e24
lakshika <lakshikamedhavini@gmail.com>
2022-06-04 14:50:47 +0530

    2nd test

--- tests/userRouteTest.js
+++ tests/userRouteTest.js
@@ -2,8 +2,10 @@ const app = require("../server.js");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 
-//get user data
+//get user data when login is successful
 test("getUserDetails", async () => {
-  const user = await supertest(server).get("/login").send({});
+  const user = await supertest(app).get("/userDetails").send({});
   expect(user.statusCode).toBe(200);
 });
+
+//
